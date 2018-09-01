a575a9ae43a4d25d90d369ea560adfcc0c06792c
illusioninlost <rchang9999@yahoo.com>
2018-09-01 13:39:54 -0700

    add items

---
+++ app/views/items/_item.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! item, :id, :name, :value, :weight, :location, :created_at, :updated_at
+json.url item_url(item, format: :json)
---
+++ db/migrate/20180901203132_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.real :value
+      t.integer :weight
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
