7f5f5bc13bb5cff035019776045e524758a64b42
illusioninlost <rchang9999@yahoo.com>
2018-09-01 17:14:14 -0700

    fix database

--- db/migrate/20180901203132_create_items.rb
+++ db/migrate/20180901203132_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
-      t.real :value
+      t.float :value
       t.integer :weight
       t.string :location
 
