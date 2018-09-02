2cbfa7c294aaf5002eb43aaed6f9b19f3a325f3d
illusioninlost <rchang9999@yahoo.com>
2018-09-02 11:16:06 -0700

    add user

---
+++ db/migrate/20180902161206_create_farmers.rb
@@ -0,0 +1,11 @@
+class CreateFarmers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :farmers do |t|
+      t.string :username
+      t.string :name
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
