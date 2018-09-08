66bbaadd3ee9ffcc1d63fdb177f1d34652ca9400
illusioninlost <rchang9999@yahoo.com>
2018-09-07 17:55:06 -0700

    add seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_161206) do
+ActiveRecord::Schema.define(version: 2018_09_08_002116) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "farmers", force: :cascade do |t|
     t.string "username"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Farmer.create!(username:"richard", name:"richard chang", password:"password")
+Farmer.create!(username:"dan", name:"dan kane", password:"password1")
+Farmer.create!(username:"likable", name:"uberdanger", password:"password2")
+
+Comment.create!(content:"The tomatos are good.")
+Comment.create!(content:"The corn is expensive.")
+Comment.create!(content:"Lots of fresh food as always.")
+
+Item.create!(name:"tomato", value: "1.23", weight: "100", location: "Red Hollow")
+Item.create!(name:"potato", value: "0.50", weight: "200", location: "Grounds Up Farm")
+Item.create!(name:"lemon", value: "0.33", weight: "300", location: "Yellow Farm")
+
