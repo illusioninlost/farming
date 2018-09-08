e3a8bdd09dae6acc02e62120e5ed409c42a36565
illusioninlost <rchang9999@yahoo.com>
2018-09-08 12:42:53 -0700

    added validations

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,3 +1,7 @@
 class Farmer < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, length: 3..20, uniqueness: true
+    validates :password, length: 3..20, presence: true
+    validates :name, presence: true, uniqueness: true
+    
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 30 }
+    validates :value, presence: true, numericality: true, length: { in: 1..20 }
+    validates :weight, presence: true, numericality: true, length: { in: 1..20 }
+    validates :location, presence: true, length: { maximum: 30 }, uniqueness: true
 end
