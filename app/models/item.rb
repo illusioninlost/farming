d81e6c4746b2c1b6ee68eddee06b45c00ca56c0d
illusioninlost <rchang9999@yahoo.com>
2018-09-08 18:19:46 -0700

    add farmer and item association

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
     validates :value, presence: true, numericality: true, length: { in: 1..20 }
     validates :weight, presence: true, numericality: true, length: { in: 1..20 }
     validates :location, presence: true, length: { maximum: 30 }, uniqueness: true
+    belongs_to :farmer
+
+
 end
